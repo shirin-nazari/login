e81cea5a709e6b26cd5b842c108302e600edc049
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2021-06-05 15:42:03 +0430

    login

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const container = document.querySelector(".container-fluid");
 const formMain = document.createElement("form");
 formMain.className = "form";
+let arrayObj = [];
+const header = document.querySelector("header");
+/* -------------------------------------------------------------------------- */
+/*                          div input ,label UserName                         */
+/* -------------------------------------------------------------------------- */
 const divUser = document.createElement("div");
 const labelUser = document.createElement("label");
 
@@ -12,9 +17,13 @@ divUser.className = "mb-3";
 labelUser.className = "form-label";
 inputUser.className = "form-control";
 inputUser.type = "text";
+labelUser.append("UserName:");
 divUser.append(labelUser);
 divUser.append(inputUser);
-labelUser.append("UserName:");
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                        div , input password , label                        */
+/* -------------------------------------------------------------------------- */
 const divPass = document.createElement("div");
 const labelPass = document.createElement("label");
 const inputPass = document.createElement("input");
@@ -28,12 +37,49 @@ inputPass.className = "form-control";
 divPass.className = "mb-3";
 divPass.append(labelPass);
 divPass.append(inputPass);
+/* -------------------------------------------------------------------------- */
+/* ------------------------------ div button , ------------------------------ */
 const divBtn = document.createElement("div");
 const btnSumbit = document.createElement("button");
 divBtn.className = "col-12";
 btnSumbit.type = "submit";
 btnSumbit.className = "btn btn-primary";
 btnSumbit.innerHTML = "Login";
+/* -------------------------------------------------------------------------- */
+/*                           addEventListener button                          */
+/* -------------------------------------------------------------------------- */
+const divAlart = document.createElement("div");
+formMain.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const valueUser = inputUser.value;
+  const valuePass = inputPass.value;
+  // for(let element in )
+
+  valueUser === "" && valuePass === ""
+    ? console.log("khali")
+    : (objData = { userName: valueUser, password: valuePass });
+
+  arrayObj.push(objData);
+  console.log(arrayObj);
+  inputUser.value = "";
+  inputPass.value = "";
+  divAlart.className = "alert alert-danger";
+  divAlart.role = "alert";
+  valueUser === "" && valuePass === ""
+    ? (divAlart.innerHTML = "empty😡😠")
+    : alert(` welcome ${objData.userName} ${objData.password}`);
+  setTimeout(() => {
+    divAlart.remove();
+  }, 1000);
+  // const li = document.createElement("li");
+  // li.innerHTML = ;
+  // container.append(li);
+  header.append(divAlart);
+});
+// divAlart.remove();
+/* -------------------------------------------------------------------------- */
+/* ----------------------------- //div button , ----------------------------- */
 divBtn.append(btnSumbit);
 formMain.append(divUser);
 formMain.append(divPass);
